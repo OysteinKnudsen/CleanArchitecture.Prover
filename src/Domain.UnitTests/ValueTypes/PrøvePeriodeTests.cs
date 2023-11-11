fe9ce322b21bd85aace3187fc26286de0beb741d
Øystein Knudsen <oystein.knudsen@bekk.no>
2023-11-11 15:55:19 +0100

    chore: add unit tests for PrøvePeriode value type

---
+++ src/Domain.UnitTests/ValueTypes/PrøvePeriodeTests.cs
@@ -0,0 +1,35 @@
+using CleanArchitecture.Prover.Domain.ValueTypes;
+
+namespace Domain.UnitTests.ValueTypes;
+
+public class PrøvePeriodeTests
+{
+    [Test]
+    public void Constructor_ValidStartAndSlutt_ShouldCreateObject()
+    {
+        var start = new DateTimeOffset(2021, 1, 1, 0, 0, 0, TimeSpan.Zero);
+        var slutt = start.AddDays(30);
+
+        var prøvePeriode = new PrøvePeriode(start, slutt);
+
+        Assert.That(prøvePeriode.Start, Is.EqualTo(start));
+        Assert.That(prøvePeriode.Slutt, Is.EqualTo(slutt));
+    }
+
+    [Test]
+    public void Constructor_StartAfterSlutt_ShouldThrowArgumentException()
+    {
+        var start = new DateTimeOffset(2021, 1, 31, 0, 0, 0, TimeSpan.Zero);
+        var slutt = new DateTimeOffset(2021, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+        Assert.Throws<ArgumentException>(() => new PrøvePeriode(start, slutt));
+    }
+
+    [Test]
+    public void Constructor_StartEqualsSlutt_ShouldThrowArgumentException()
+    {
+        var startAndSlutt = new DateTimeOffset(2021, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+        Assert.Throws<ArgumentException>(() => new PrøvePeriode(startAndSlutt, startAndSlutt));
+    }
+}
--- src/Domain/ValueTypes/PrøvePeriode.cs
+++ src/Domain/ValueTypes/PrøvePeriode.cs
@@ -1,3 +1,18 @@
 namespace CleanArchitecture.Prover.Domain.ValueTypes;
 
-public record PrøvePeriode(DateTimeOffset Start, DateTimeOffset Slutt);
+public record PrøvePeriode
+{
+    public DateTimeOffset Start { get; }
+    public DateTimeOffset Slutt { get; }
+
+    public PrøvePeriode(DateTimeOffset Start, DateTimeOffset Slutt)
+    {
+        if (Start >= Slutt)
+        {
+           throw new ArgumentException($"{nameof(Start)} must be before {nameof(Slutt)}");
+        }
+
+        this.Start = Start;
+        this.Slutt = Slutt;
+    }
+}
