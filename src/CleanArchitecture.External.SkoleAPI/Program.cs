702ee442f201241326d47750319c493a09083fbd
Øystein Knudsen <oystein.knudsen@bekk.no>
2023-11-11 15:17:32 +0100

    chore: add document titles to SwaggerUI

--- src/CleanArchitecture.External.SkoleAPI/Program.cs
+++ src/CleanArchitecture.External.SkoleAPI/Program.cs
@@ -13,7 +13,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.DocumentTitle = "Skole API");
 }
 
 app.UseHttpsRedirection();
