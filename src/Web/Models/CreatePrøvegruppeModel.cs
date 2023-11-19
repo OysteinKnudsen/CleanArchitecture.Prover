e75aeb7236d2785dfeddb7a682feaded352fd23d
Øystein Knudsen <oystein.knudsen@bekk.no>
2023-11-19 20:12:10 +0100

    Fjern irrelevant todo

--- src/Web/Models/CreatePrøvegruppeModel.cs
+++ src/Web/Models/CreatePrøvegruppeModel.cs
@@ -1,4 +1,3 @@
 namespace CleanArchitecture.Prover.Web.Models;
 
 public record CreatePrøvegruppeModel(int PrøveId, int LærerId, IEnumerable<int> Elever);
-// TODO: Utvid denne modellen til å inkludere nødvendig data for å opprette en prøvegruppe
