28c57df1669a7201c57d8c330d37032c38bc455b
Øystein Knudsen <oystein.knudsen@bekk.no>
2023-11-22 19:04:47 +0100

    Legg til litt hint om DI

--- src/Application/DependencyInjection.cs
+++ src/Application/DependencyInjection.cs
@@ -7,6 +7,14 @@ namespace CleanArchitecture.Prover.Application;
 
 public static class DependencyInjection
 {
+    
+    /*
+     * Når en klasse krever en avhengighet, injiseres den automatisk av DI-kontaineren
+     * basert på konfigurasjonen i IServiceCollection. DI-kontaineren håndterer
+     * altså opprettelse og levetid av avhengigheter. Dette bidrar til "loose coupling".
+     *
+     * Hint: Har du opprettet en ny service? Husk at du må registrere den i DI-kontaineren.
+     */
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddTransient<IPrøvegruppeService, PrøvegruppeService>();
