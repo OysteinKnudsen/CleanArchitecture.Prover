0c6990197ec4cc642f9c368f8646a4989a79b26a
Øystein Knudsen <oystein.knudsen@bekk.no>
2023-11-22 18:31:03 +0100

    Todo på tests

--- src/Application.UnitTests/Prøvegrupper/PrøvegruppeServiceTests.cs
+++ src/Application.UnitTests/Prøvegrupper/PrøvegruppeServiceTests.cs
@@ -1,11 +1,16 @@
-using CleanArchitecture.Prover.Application.Prøvegrupper.Services;
-
-namespace Application.UnitTests.Prøver;
+namespace Application.UnitTests.Prøvegrupper;
 
 public class PrøvegruppeServiceTests
 {
+    /*
+     * Dette er bare starten på en testklasse med ett eksempel på hva som kan bli testet.
+     * Poenget er å få demonstrert effekten av å ha tydelig definerte grensesnitt og at det er enkelt å teste.
+     */
+    
     [Fact]
-    public void Test1()
+    public void UpdateStatusAsync_WhenOutsidePrøveperiode_DoesNotUpdateStatus()
     {
+        // TODO: Skriv en test som verifiserer at UpdateStatusAsync ikke oppdaterer status på en prøvegruppe som er utenfor prøveperioden.
+        // Hint: Her må du mocke ut avhengighetene til PrøvegruppeService og verifisere at status ikke blir oppdatert. 
     }
 }
