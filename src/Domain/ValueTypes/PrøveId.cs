7d8bb40f4105ff98e633e1a9c0b58099625e39f0
Henrik Wingerei <henrik.wingerei@gmail.com>
2023-12-11 21:24:59 +0100

    Endre tilbake til int for PrøveId

--- src/Domain/ValueTypes/PrøveId.cs
+++ src/Domain/ValueTypes/PrøveId.cs
@@ -1,7 +1,7 @@
 namespace CleanArchitecture.Prover.Domain.ValueTypes;
 
-public record PrøveId(string Id)
+public record PrøveId(int Id)
 {
-    public static explicit operator PrøveId(string id) => new(id);
-    public static implicit operator string(PrøveId prøveId) => prøveId.Id;
+    public static explicit operator PrøveId(int id) => new(id);
+    public static implicit operator int(PrøveId prøveId) => prøveId.Id;
 }
